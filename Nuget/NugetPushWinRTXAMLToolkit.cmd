283287c6de36c681f035766536a7c51ec1f777a7
Filip Skakun <filip.skakun@gmail.com>
2013-06-14 08:29:49 -0700

    Version 1.4.4.1 Calendar fix for dark theme.

--- Nuget/NugetPushWinRTXAMLToolkit.cmd
+++ Nuget/NugetPushWinRTXAMLToolkit.cmd
@@ -1,5 +1,5 @@
-nuget push WinRTXamlToolkit.1.4.4.0.nupkg
-nuget push WinRTXamlToolkit.Composition.1.4.4.0.nupkg
-nuget push WinRTXamlToolkit.Controls.Calendar.1.4.4.0.nupkg
-nuget push WinRTXamlToolkit.Controls.DataVisualization.1.4.4.0.nupkg
-nuget push WinRTXamlToolkit.Debugging.1.4.4.0.nupkg
+nuget push WinRTXamlToolkit.1.4.4.1.nupkg
+nuget push WinRTXamlToolkit.Composition.1.4.4.1.nupkg
+nuget push WinRTXamlToolkit.Controls.Calendar.1.4.4.1.nupkg
+nuget push WinRTXamlToolkit.Controls.DataVisualization.1.4.4.1.nupkg
+nuget push WinRTXamlToolkit.Debugging.1.4.4.1.nupkg
