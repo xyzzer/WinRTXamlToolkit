c2b7900a9c473ed471f3e4a7835bb421a451781d
snd\xyzzer_cp <filip.skakun@gmail.com>
2014-09-13 23:24:47 -0700

    Disabled pushing the WinRT Proxy for debugging tools since that isn't done yet.

--- Nuget/push.cmd
+++ Nuget/push.cmd
@@ -1,6 +1,6 @@
 nuget push WinRTXamlToolkit.Windows.1.7.2.0.nupkg
 nuget push WinRTXamlToolkit.Debugging.Windows.1.7.2.0.nupkg
-nuget push WinRTXamlToolkit.Debugging.WinRTProxy.Windows.1.7.2.0.nupkg
+@rem nuget push WinRTXamlToolkit.Debugging.WinRTProxy.Windows.1.7.2.0.nupkg
 nuget push WinRTXamlToolkit.Controls.Calendar.Windows.1.7.2.0.nupkg
 nuget push WinRTXamlToolkit.Controls.DataVisualization.Windows.1.7.2.0.nupkg
 nuget push WinRTXamlToolkit.Controls.Gauge.Windows.1.7.2.0.nupkg
