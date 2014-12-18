bc45e5b66ea23ed9ff829c493eaeb9b173b8dacd
Filip Skakun <Filip.Skakun@microsoft.com>
2014-12-18 04:27:37 -0800

    1.7.8.0

--- Nuget/push.cmd
+++ Nuget/push.cmd
@@ -1,12 +1,12 @@
-nuget push WinRTXamlToolkit.Windows.1.7.7.0.nupkg
-nuget push WinRTXamlToolkit.Debugging.Windows.1.7.7.0.nupkg
-@rem nuget push WinRTXamlToolkit.Debugging.WinRTProxy.Windows.1.7.7.0.nupkg
-nuget push WinRTXamlToolkit.Controls.Calendar.Windows.1.7.7.0.nupkg
-nuget push WinRTXamlToolkit.Controls.DataVisualization.Windows.1.7.7.0.nupkg
-nuget push WinRTXamlToolkit.Controls.Gauge.Windows.1.7.7.0.nupkg
+nuget push WinRTXamlToolkit.Windows.1.7.8.0.nupkg
+nuget push WinRTXamlToolkit.Debugging.Windows.1.7.8.0.nupkg
+@rem nuget push WinRTXamlToolkit.Debugging.WinRTProxy.Windows.1.7.8.0.nupkg
+nuget push WinRTXamlToolkit.Controls.Calendar.Windows.1.7.8.0.nupkg
+nuget push WinRTXamlToolkit.Controls.DataVisualization.Windows.1.7.8.0.nupkg
+nuget push WinRTXamlToolkit.Controls.Gauge.Windows.1.7.8.0.nupkg
 
-nuget push WinRTXamlToolkit.WindowsPhone.1.7.7.0.nupkg
-nuget push WinRTXamlToolkit.Debugging.WindowsPhone.1.7.7.0.nupkg
-nuget push WinRTXamlToolkit.Controls.Calendar.WindowsPhone.1.7.7.0.nupkg
-nuget push WinRTXamlToolkit.Controls.DataVisualization.WindowsPhone.1.7.7.0.nupkg
-nuget push WinRTXamlToolkit.Controls.Gauge.WindowsPhone.1.7.7.0.nupkg
+nuget push WinRTXamlToolkit.WindowsPhone.1.7.8.0.nupkg
+nuget push WinRTXamlToolkit.Debugging.WindowsPhone.1.7.8.0.nupkg
+nuget push WinRTXamlToolkit.Controls.Calendar.WindowsPhone.1.7.8.0.nupkg
+nuget push WinRTXamlToolkit.Controls.DataVisualization.WindowsPhone.1.7.8.0.nupkg
+nuget push WinRTXamlToolkit.Controls.Gauge.WindowsPhone.1.7.8.0.nupkg
