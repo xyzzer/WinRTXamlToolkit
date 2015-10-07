802d6ab583c014dfabfb293ac6542cae77364ac1
Filip Skakun (WINDOWS PHOTOS) <Filip.Skakun@microsoft.com>
2015-10-06 23:13:34 -0700

    1.9.2 -> 1.9.3

--- Nuget/push.cmd
+++ Nuget/push.cmd
@@ -1,5 +1,5 @@
-nuget push WinRTXamlToolkit.UWP.1.9.2.nupkg
-nuget push WinRTXamlToolkit.Debugging.UWP.1.9.2.nupkg
-nuget push WinRTXamlToolkit.Controls.Calendar.UWP.1.9.2.nupkg
-nuget push WinRTXamlToolkit.Controls.DataVisualization.UWP.1.9.2.nupkg
-nuget push WinRTXamlToolkit.Controls.Gauge.UWP.1.9.2.nupkg
+nuget push WinRTXamlToolkit.UWP.1.9.3.nupkg
+nuget push WinRTXamlToolkit.Debugging.UWP.1.9.3.nupkg
+nuget push WinRTXamlToolkit.Controls.Calendar.UWP.1.9.3.nupkg
+nuget push WinRTXamlToolkit.Controls.DataVisualization.UWP.1.9.3.nupkg
+nuget push WinRTXamlToolkit.Controls.Gauge.UWP.1.9.3.nupkg
