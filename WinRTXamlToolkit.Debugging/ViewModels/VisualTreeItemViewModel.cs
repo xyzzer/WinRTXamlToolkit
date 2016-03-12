9d04dc2d7b1848a1ea0381a0fcd586841cb5f0da
Filip Skakun <filip.skakun@gmail.com>
2016-03-12 13:55:51 -0800

    Missed one

---
+++ WinRTXamlToolkit.Debugging/ViewModels/VisualTreeItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WinRTXamlToolkit.Debugging.ViewModels
+{
+    /// <summary>
+    /// Base class of tree items in the VisualTreeViewModel.
+    /// </summary>
+    /// <remarks>
+    /// Base class for DependencyObjectViewModel and StubTreeItemViewModel.
+    /// </remarks>
+    public class VisualTreeItemViewModel : TreeItemViewModel
+    {
+        public VisualTreeViewModel VisualTreeViewModel => (VisualTreeViewModel)this.TreeViewModel;
+
+        public VisualTreeItemViewModel(
+            VisualTreeViewModel treeViewViewModel,
+            VisualTreeItemViewModel parent) : base(treeViewViewModel, parent)
+        {
+        }
+    }
+}
