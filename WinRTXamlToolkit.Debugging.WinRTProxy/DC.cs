48c91d3393c91148db204b3aec548d3322703c15
Filip Skakun <filips@microsoft.com>
2018-01-23 14:56:57 -0800

    Added a WinRT proxy WinRT component project

---
+++ WinRTXamlToolkit.Debugging.WinRTProxy/DC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+
+namespace WinRTXamlToolkit.Debugging.WinRTProxy
+{
+    public static class DC
+    {
+        public static void Show()
+        {
+            Debugging.DC.Show();
+        }
+
+        public static void ShowLog()
+        {
+            Debugging.DC.ShowLog();
+        }
+
+        public static void Trace(object value)
+        {
+            Debugging.DC.Trace(value);
+        }
+
+        public static IAsyncAction ShowVisualTreeAsync()
+        {
+            return Debugging.DC.ShowVisualTreeAsync().AsAsyncAction();
+        }
+
+        public static IAsyncAction ShowVisualTreeAsync(UIElement element)
+        {
+            return Debugging.DC.ShowVisualTreeAsync(element).AsAsyncAction();
+        }
+    }
+}
